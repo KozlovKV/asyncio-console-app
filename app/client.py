36f6a3413851f0e6aad5b7eeedcd51967330fec9
Kirill <kozlov.kirill.v.work@gmail.com>
2021-12-17 19:37:10 +0600

    Added docstrings, moved data formatting for get method from server to client, added strict args in methods with exception in loop method

--- app/client.py
+++ app/client.py
@@ -1,47 +1,92 @@
+"""
+Клиентская часть приложения.
+Отвечает за приём данных от пользователя, проверку их корректности, отправку на сервер и вывод результата
+"""
+from app.server import Server
+
+
 class Client:
+    """
+    `COMMANDS` - словарь ключевых слов, каждому из которых соответствует лямбда-функция,
+    запускающая необходимый метод для обработки данной команды
+    """
     COMMANDS = {
-        'get': lambda self, args: self.get(args),
-        'put': lambda self, args: self.put(args),
+        'get': lambda self, args: self.get(*args[1:]),
+        'put': lambda self, args: self.put(*args[1:]),
         'error': lambda self, args: self.error(),
     }
 
-    def __init__(self, server):
+    def __init__(self, server: Server) -> None:
         self.server = server
 
-    def loop(self):
+    def loop(self) -> None:
+        """
+        Основная "петля" приложения.
+
+        Принимает команды от пользователя, делит их по пробелам и ищет команду в словаре `COMMANDS`.
+        В случае пустого ввода, неверного количества аргументов или
+         некорректной команды вызывает метод `Client.error()`
+        """
         while True:
             args = input().strip().split()
             try:
                 command = self.COMMANDS.get(args[0], self.COMMANDS['error'])
                 command(self, args)
+            except TypeError:
+                self.error()
             except IndexError:
                 self.error()
 
-    def get(self, args):
-        if len(args) != 2:
+    def get(self, key: str) -> None:
+        """
+        Метод вывода полученных от сервера данных.
+
+        Преобразует полученные данные из списка пар [ключ, все_записи]
+        в простой список с тройками [ключ, значение, время].
+        :param key: Ключ, по которому получаются данные с севера.
+        Если `key = '*'`, выводит все имеющиеся на сервере данные.
+        Если ключа в словаре нет, вызывается метод `Client.error()`.
+        :type key: `str`
+        """
+        data = self.server.get(key)
+        if data == -1:
             self.error()
             return
-        data = self.server.get(args)
         result = []
         for note in data:
-            for values in note[1]:
-                result.append(f'{note[0]} {values[0]} {values[1]}')
+            key = note[0]
+            values = note[1]
+            for value in values:
+                result.append(f'{key} {value[0]} {value[1]}')
         print('\n'.join(result))
 
-    def put(self, args):
-        if len(args) != 4:
-            self.error()
-            return
+    def put(self, key: str, data: str, time: str) -> None:
+        """
+        Метод записи данных на сервер.
+
+        :param key: Ключ, по которому добавляется новая запись.
+        :type key: `str`
+
+        :param data: Записываемое значение.
+        :type data: `str`, после преобразования должно быть `float`,
+        если преобразование невозможно, вызывается метод `Client.error()`
+
+        :param time: Время записи (вводится произвольно).
+        :type data: `str`, после преобразования должно быть `int`,
+        если преобразование невозможно, вызывается метод `Client.error()`
+        """
         try:
-            args[2] = float(args[2])
-            args[3] = int(args[3])
+            data = float(data)
+            time = int(time)
         except ValueError:
             self.error()
             return
-        self.server.put(args)
+        self.server.put(key, data, time)
         print('ok')
 
     @staticmethod
     def error():
+        """
+        Статический метод вывода сообщения об ошибке
+        """
         print('\nerror\nerror command\n')
-
--- app/server.py
+++ app/server.py
@@ -1,40 +1,73 @@
+"""
+Серверная часть приложения.
+Получает от пользователя очищенные данные и производит манипуляции с хранящимися на сервере данными.
+"""
 import json
 import os.path
 
 
 class Server:
+    """
+    `DATA_URL` - строка относительного адреса файла словаря относительно файла запуска программы (`run.py`).
+    """
     DATA_URL = './data.json'
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.check_json()
 
-    def check_json(self):
+    def check_json(self) -> None:
+        """
+        Проверяет, есть ли уже файл словаря по адресу `Server.DATA_URL`.
+        Если файла нет, создаёт его.
+        """
         if not os.path.exists(self.DATA_URL):
             with open(self.DATA_URL, 'x') as file:
                 file.write('{}')
 
     @property
-    def data(self):
+    def data(self) -> dict:
+        """
+        Метод-свойство для получения всех данных из словаря.
+        :return: Словарь из файла по адресу `Server.DATA_URL`.
+        """
         with open(self.DATA_URL, 'r') as file:
             data = json.load(file)
             return data
 
-    def get(self, args):
-        data = self.data
-        result = []
-        if args[1] == '*':
-            for key in data.keys():
-                result.append([key, data[key]])
-        elif data.get(args[1], 'error') == 'error':
-            return '\nerror\nerror command\n'
-        else:
-            result.append([args[1], data[args[1]]])
-        return result
+    def get(self, key: str) -> [list, int]:
+        """
+        Получение данных по ключу
 
-    def put(self, args):
+        :param key: Ключ, по которому получаются данные из словаря.
+        Если `key = '*'`, возвращает все имеющиеся на сервере данные.
+        Если ключа в словаре нет, возвращает -1.
+        :type key: `str`
+        :return: -1 либо список из словарей с ключами и всеми данными, соответствующими этим ключам.
+        """
+        data = self.data
+        if key == '*':
+            return list(data.items())
+        elif data.get(key, 'error') == 'error':
+            return -1
+        else:
+            return [[key, data[key]], ]
+
+    def put(self, key: str, data: float, time: int) -> None:
+        """
+        Добавляет в словарь новую запись.
+
+        :param key: Ключ, по которому добавляется новая запись.
+        :type key: `str`
+
+        :param data: Записываемое значение.
+        :type data: `float`
+
+        :param time: Время записи (вводится произвольно).
+        :type data: `int`
+        """
         new_data = self.data
-        value_list = new_data.get(args[1], [])
-        value_list += [[args[2], args[3]], ]
-        new_data[args[1]] = value_list
+        value_list = new_data.get(key, [])
+        value_list += [[data, time], ]
+        new_data[key] = value_list
         with open(self.DATA_URL, 'w') as file:
             json.dump(new_data, file)
